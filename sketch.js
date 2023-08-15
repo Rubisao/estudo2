21180bc17b2bca2eb130342b670bed6532ae5b6a
Rubisao <118467023+Rubisao@users.noreply.github.com>
2023-08-14 22:51:09 -0300

    Add files via upload
    Primeiras aulas

---
+++ sketch.js
@@ -0,0 +1,49 @@
+let imagemDaEstrada;
+let imagemAtor;
+let imagemCarro1;
+
+//function 
+let xCarro = 600;
+let yCarro = 40;
+
+//Ator
+let yAtor = 366;
+
+function preload(){
+  imagemDaEstrada = loadImage("Imagens/estrada.png")
+  imagemAtor = loadImage("Imagens/ator-1.png")
+  imagemCarro1 = loadImage("Imagens/carro-1.png")
+}
+
+function setup() {
+  createCanvas(500, 400);
+}
+
+function draw() {
+  background(imagemDaEstrada);
+  mostraAtor();
+  mostraCarro();
+  movimentaCarro();
+  movimentoAtor();
+}
+
+//Funções
+function mostraAtor(){
+    image(imagemAtor, 100, yAtor, 30, 30);
+}
+
+function mostraCarro(){
+    image(imagemCarro1, xCarro, yCarro, 50, 40);
+}
+
+function movimentaCarro(){
+  xCarro -= 2;
+}
+
+function movimentoAtor(){
+  if (keyIsDown(UP_ARROW)){
+    yAtor -= 3;
+  }
+  if (keyIsDown(DOWN_ARROW))
+    yAtor += 3;
+}
