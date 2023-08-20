f488d97fde9b0f4832bef197ae7a3156c6e82794
Rubisao <118467023+Rubisao@users.noreply.github.com>
2023-08-19 22:38:40 -0300

    Update imagens.js

--- imagens.js
+++ imagens.js
@@ -5,10 +5,10 @@ let imagemCarro2;
 let imagemCarro3;
 
 function preload(){
-  imagemDaEstrada = loadImage("Imagens/estrada.png")
-  imagemAtor = loadImage("Imagens/ator-1.png")
-  imagemCarro1 = loadImage("Imagens/carro-1.png")
-  imagemCarro2 = loadImage("Imagens/carro-2.png")
-  imagemCarro3 = loadImage("Imagens/carro-3.png")
-  imagemCarros = [imagemCarro1, imagemCarro2, imagemCarro3]
+  imagemDaEstrada = loadImage("Imagens/estrada.png");
+  imagemAtor = loadImage("Imagens/ator-1.png");
+  imagemCarro1 = loadImage("Imagens/carro-1.png");
+  imagemCarro2 = loadImage("Imagens/carro-2.png");
+  imagemCarro3 = loadImage("Imagens/carro-3.png");
+  imagemCarros = [imagemCarro1, imagemCarro2, imagemCarro3];
 }
