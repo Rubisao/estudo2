e0472e9caaa78bbd6169a7f27be82de0be730bf3
Rubisao <118467023+Rubisao@users.noreply.github.com>
2023-08-19 22:35:57 -0300

    Create imagens.js
    Deixando o código mais limpo e organizado

---
+++ imagens.js
@@ -0,0 +1,14 @@
+let imagemDaEstrada;
+let imagemAtor;
+let imagemCarro1;
+let imagemCarro2;
+let imagemCarro3;
+
+function preload(){
+  imagemDaEstrada = loadImage("Imagens/estrada.png")
+  imagemAtor = loadImage("Imagens/ator-1.png")
+  imagemCarro1 = loadImage("Imagens/carro-1.png")
+  imagemCarro2 = loadImage("Imagens/carro-2.png")
+  imagemCarro3 = loadImage("Imagens/carro-3.png")
+  imagemCarros = [imagemCarro1, imagemCarro2, imagemCarro3]
+}
