2dc6b6e613aece3e97edc0b76e36f9148db59fdb
Rubisao <118467023+Rubisao@users.noreply.github.com>
2023-08-20 22:34:37 -0300

    Update imagens.js
    Adicionando sons e novos carros

--- imagens.js
+++ imagens.js
@@ -4,11 +4,19 @@ let imagemCarro1;
 let imagemCarro2;
 let imagemCarro3;
 
+let trilhaSonora;
+let somColisao;
+let somPontos;
+
 function preload(){
-  imagemDaEstrada = loadImage("Imagens/estrada.png");
-  imagemAtor = loadImage("Imagens/ator-1.png");
-  imagemCarro1 = loadImage("Imagens/carro-1.png");
-  imagemCarro2 = loadImage("Imagens/carro-2.png");
-  imagemCarro3 = loadImage("Imagens/carro-3.png");
-  imagemCarros = [imagemCarro1, imagemCarro2, imagemCarro3];
+  imagemDaEstrada = loadImage("Imagens/estrada.png")
+  imagemAtor = loadImage("Imagens/ator-1.png")
+  imagemCarro1 = loadImage("Imagens/carro-1.png")
+  imagemCarro2 = loadImage("Imagens/carro-2.png")
+  imagemCarro3 = loadImage("Imagens/carro-3.png")
+  imagemCarros = [imagemCarro1, imagemCarro2, imagemCarro3, imagemCarro1, imagemCarro2, imagemCarro3]
+  
+  trilhaSonora = loadSound("Sons/trilha.mp3");
+  somColisao = loadSound("Sons/colidiu.mp3");
+  somPontos = loadSound("Sons/pontos.wav");
 }
