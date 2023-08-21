34bdfaa0f46e36247f85f07ee1ec0becf5afbb51
Rubisao <118467023+Rubisao@users.noreply.github.com>
2023-08-20 22:33:51 -0300

    Update ator.js
    Criação de novas funções para o ator.

--- ator.js
+++ ator.js
@@ -1,6 +1,7 @@
 let yAtor = 366;
-let xAtor = 100;
-
+let xAtor = 85;
+let colisao = false;
+let meusPontos = 0;
 
 function mostraAtor(){
     image(imagemAtor, xAtor, yAtor, 30, 30);
@@ -11,5 +12,47 @@ function movimentoAtor(){
     yAtor -= 3;
   }
   if (keyIsDown(DOWN_ARROW))
-    yAtor += 3;
+    if(podeSeMover()){
+      yAtor += 3;
+    }
+}
+
+function verificaColisao(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    colisao = collideRectCircle(xCarros[i], yCarros[i], comprimentoCarro, alturaCarro, xAtor, yAtor, 15) 
+  if (colisao){
+    voltaInicio();
+    somColisao.play();
+    if(pontosMaiorQueZero()){  
+      meusPontos -= 1;
+      }
+    }
+  }
+}
+
+function voltaInicio(){
+  yAtor = 366;
+}
+
+function incluiPontos(){
+  textAlign(CENTER);
+  textSize(25);
+  fill(240,230,140);
+  text(meusPontos, width / 5, 27);
+}
+
+function marcaPonto(){
+  if (yAtor < 15){
+    meusPontos += 1;
+    somPontos.play();
+    voltaInicio();
+  }
+}
+
+function pontosMaiorQueZero(){
+  return meusPontos > 0;
+}
+
+function podeSeMover(){
+  return yAtor < 366;
 }
